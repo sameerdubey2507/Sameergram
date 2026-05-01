4a62489227d29557e746ff540097c387a34da62a
sameerdubey2507 <craftedlegend25@gmail.com>
2026-05-01 17:40:25 +0530

    Update server configuration for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 })
